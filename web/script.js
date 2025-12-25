b84775ecebe0c0a844b21651213d69898570ef65
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-12-25 18:46:28 +0100

    changed upload logic to use xhr and track upload progress

--- web/script.js
+++ web/script.js
@@ -1,8 +1,10 @@
-let fileInput, uploadBtn;
+let fileInput, uploadBtn, progressBar, statusEl;
 
 document.addEventListener("DOMContentLoaded", () => {
   fileInput = document.getElementById("file");
   uploadBtn = document.getElementById("upload-btn");
+  progressBar = document.getElementById("progress-bar");
+  statusEl = document.getElementById("status");
 
   uploadBtn.addEventListener("click", handleUpload);
 });
@@ -18,14 +20,38 @@ async function handleUpload(e) {
   const formData = new FormData();
   formData.append("file", file);
 
-  try {
-    const response = await fetch("/upload", {
-      method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+  progressBar.style.opacity = 100;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.upload.addEventListener("progress", (e) => {
+    if (e.lengthComputable) {
+      const percentCompleted = Math.round((e.loaded / e.total) * 100);
+
+      progressBar.value = percentCompleted;
+      statusEl.textContent = `${percentCompleted}% uploaded`;
+    }
+  });
+
+  const completeHandler = (e) => {
+    statusEl.textContent = "Upload Completed";
+    setTimeout(() => {
+      progressBar.style.opacity = 0;
+    }, 1000);
+  };
+  const errorHandler = (e) => {
+    statusEl.textContent = "Upload Failed";
+  };
+  const abortHandler = (e) => {
+    statusEl.textContent = "Upload Aborted";
+  };
+
+  xhr.addEventListener("load", completeHandler, false);
+  xhr.addEventListener("error", errorHandler, false);
+  xhr.addEventListener("abort", abortHandler, false);
+
+  xhr.open("POST", "/upload");
+  xhr.send(formData);
+
+  console.log(xhr.response);
 }
